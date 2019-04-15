46c1bfa893d10a4b78e3422372b1fa8779dd3c06
KhallilB <khallilbailey@gmail.com>
2019-04-15 15:22:08 -0700

    init variables for landing screen functionality

--- Assets/Scripts/LandingScreen.cs
+++ Assets/Scripts/LandingScreen.cs
@@ -2,8 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]                         // Add audio source when attaching the script
 public class LandingScreen : MonoBehaviour
 {
+    // Landing Screen inspector variables
+    public Texture2D _landingScreenBackground;                  // Create slot in inspector to assign landing screen background image
+    public Texture2D _landingScreenText;                        // Create slot in inspector to assign landing screen text
+
+    // Landing Screen audio inspector variables
+    private AudioSource _landingScreenAudio;                    // Defines name for auido source component
+    public AudioClip _landingScreenMusic;                       // Create slot in inspector to assign landing screen music
+
+    // Variables for fading items on the landing screen
+    private float _landingScreenFadeValue;                      // Defines the fade value
+    private float _landingScreenFadeSpeed = 0.15f;              // Defines the fade speed
+
+    private LandingScreenController _landingScreenController;   // Defines the naming conventions for the splash screen contollers
+
+    // Defines the states for the splash screens
+    private enum LandingScreenController
+    {
+        LandingScreenFadeIn = 0,
+        LandingScreenFadeOut = 1
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +35,15 @@ public class LandingScreen : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void LandingScreenFadeIn()
+    {
+        Debug.Log("LandingScreenFadeIn");
+    }
+
+    private void LandingScreenFadeOut()
+    {
+        Debug.Log("LandingScreenFadeOut");
     }
 }
