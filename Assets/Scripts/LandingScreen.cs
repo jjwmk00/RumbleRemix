51f27a88636c2d5ece846efaf4e2e175d6fc63a4
KhallilB <khallilbailey@gmail.com>
2019-04-15 18:23:21 -0700

    sucessful load screen an move to another

--- Assets/Scripts/LandingScreen.cs
+++ Assets/Scripts/LandingScreen.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(AudioSource))]                         // Add audio source when attaching the script
 public class LandingScreen : MonoBehaviour
 {
     // Landing Screen inspector variables
     public Texture2D _landingScreenBackground;                  // Create slot in inspector to assign landing screen background image
-    public Texture2D _landingScreenText;                        // Create slot in inspector to assign landing screen text
+    public Texture2D _landingScreenTitleText;                   // Create slot in inspector to assign landing screen title text
+    public Texture2D _landingScreenSubText;                     // Create slot in inspector to assing landing screen subtext
 
     // Landing Screen audio inspector variables
     private AudioSource _landingScreenAudio;                    // Defines name for auido source component
@@ -87,7 +89,12 @@ public class LandingScreen : MonoBehaviour
         if(_landingScreenFadeValue > 1)                                         // If fade value is greater than 1
         {
             _landingScreenFadeValue = 1;                                        // Then set fade value to one                   
-        }   
+        }
+        
+        if(_landingScreenFadeValue == 1)                                        // If fade value equals one
+        {
+            _landingScreenController = LandingScreen.LandingScreenController.LandingScreenFadeOut;  // Set splash screen controller to equal landing screen fade out
+        }
     }
 
     private void LandingScreenFadeOut()
@@ -102,5 +109,26 @@ public class LandingScreen : MonoBehaviour
         {
             _landingScreenFadeValue = 0;                                        // Then set the value to 0
         }
+
+        // To load the next scene after fade out
+        if(_landingScreenFadeValue == 0)                                        // If fade value equals 0
+        {
+            SceneManager.LoadScene("ControllerWarning");                        // Load the controller warning scene
+        }
+    }
+
+    // Draws texture on screen based on screen height and width
+    private void OnGUI()
+    {
+        // Draws the background
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), _landingScreenBackground);
+
+        GUI.color = new Color(1, 1, 1, _landingScreenFadeValue);                // GUI color is equal to black transparency is the screen fade value
+
+        // Draws the Title
+        GUI.DrawTexture(new Rect((Screen.width/4), (Screen.height/3), (Screen.width / 2), (Screen.height / 5)), _landingScreenTitleText);
+
+        // Draws the sub text
+        GUI.DrawTexture(new Rect(0, 0, (Screen.width/8), (Screen.height/11)), _landingScreenSubText);
     }
 }
