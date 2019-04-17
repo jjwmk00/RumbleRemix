d6c8c10980ef21715cc8d5738fe017106ff1136c
KhallilB <khallilbailey@gmail.com>
2019-04-17 16:08:18 -0700

    game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    private void Awake()
+    {
+        Cursor.visible = false;                                                // Set the cursor to not be visible
+        Cursor.lockState = CursorLockMode.Locked;                              // Lock the cursor
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(this);                                               // Dont destroy the game object when loading new scenes
     }
 
     // Update is called once per frame
